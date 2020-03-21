60535ae7c68f92049c13185503b94342e18c95ec
Strusinio <patrykkaszubowski@poczta.fm>
2020-03-22 00:41:40 +0100

    new activity commit

---
+++ app/src/main/java/com/example/bmicalculator/Recipe.java
@@ -0,0 +1,26 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Recipe extends AppCompatActivity {
+    private Button backToMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipe);
+
+        // BTN initialize
+        backToMain = findViewById(R.id.recipe_back);
+        backToMain.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+    }
+}
